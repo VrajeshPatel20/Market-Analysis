c027202fe48f564780795cdf7e160dcbfe9c2a28
Vrajesh Patel <61248878+VrajeshPatel20@users.noreply.github.com>
2021-05-16 01:43:13 -0400

    Update crypto.js

--- Frontend/crypto.js
+++ Frontend/crypto.js
@@ -19,3 +19,33 @@ const getData = () => {
       document.querySelector("#pics").insertAdjacentHTML("afterbegin", html);
     });
 };
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function sendEmail(){
+ 
+  var input = document.getElementById("msg").value;
+  var email = document.getElementById("email").value;
+  var name = document.getElementById("name").value;
+
+  var detail = name + "\n"+ email + "\n" + input;
+
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "cryptoalerts167@gmail.com",
+    Password:"Kaboom001$$@",
+    To: "vrajesh1@my.yorku.ca",
+    From: "cryptoalerts167@gmail.com",
+    Subject: email,
+    Body: detail
+  }).then(
+    message=>alert("mail sent successfully")
+  );
+}
+
+
