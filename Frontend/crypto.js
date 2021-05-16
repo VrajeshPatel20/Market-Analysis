896dc40c1dc3d02d51b2b4c6ad7cd3e02dc8d566
Vrajesh Patel <61248878+VrajeshPatel20@users.noreply.github.com>
2021-05-16 16:55:38 -0400

    Update crypto.js

--- Frontend/crypto.js
+++ Frontend/crypto.js
@@ -1,5 +1,26 @@
+// const getData = () => {
+//   fetch("http://127.0.0.1:5000/")
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw Error("ERROR");
+//       }
+//       return response.json();
+//     })
+//     .then((responseData) => {
+//       const array = responseData;
+//       const html = array.map((coin) => {
+//         console.log(coin);
+//         return `
+//           <div style="font-size: x-large;color: #eeee;">
+//           <p><li> Crypto : ${coin} </li></p>
+//           </div>
+//           `;
+//       });
+//       document.querySelector("#pics").insertAdjacentHTML("afterbegin", html);
+//     });
+// };
 const getData = () => {
-  fetch("http://127.0.0.1:5000/")
+  fetch("https://backend00.herokuapp.com/crypto")
     .then((response) => {
       if (!response.ok) {
         throw Error("ERROR");
@@ -8,16 +29,24 @@ const getData = () => {
     })
     .then((responseData) => {
       const array = responseData;
-      const html = array.map((coin) => {
-        console.log(coin);
-        return `
-          <div>
-          <p> Crypto : ${coin} </p>
-          </div>
-          `;
-      });
-      document.querySelector("#pics").insertAdjacentHTML("afterbegin", html);
+      results = "<table style=\"width:2000px;k\">";
+      for (var i=0; i<array.length; i+=2) {
+          results += " <tr><td><div style=\"font-size: xx-large; color: #eeee;\"><li>" + array[i] + "</li></div></td>";  
+          if(i+1<array.length){
+            results += "<td><div style=\"font-size: xx-large; color: #eeee;\"><li>" + array[i+1] + "</li></div></td></tr>";
+          }  
+      }
+      
+      results += "<table><br /> <br />";
+      
+       var div = document.getElementById("pics");
+          div.innerHTML = results;    
     });
+};
+
+
+window.onload = function() {
+  getData();
 };
 function openForm() {
   document.getElementById("myForm").style.display = "block";
